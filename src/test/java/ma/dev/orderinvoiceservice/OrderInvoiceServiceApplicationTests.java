abbcec53570acb7e3c8852fe9cc63496cdc9a685
Yassine <aqwzsxcv123@gmail.com>
2023-08-31 15:33:58 +0100

    first commit

---
+++ src/test/java/ma/dev/orderinvoiceservice/OrderInvoiceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.dev.orderinvoiceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderInvoiceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
